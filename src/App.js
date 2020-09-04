fcbf332fd291ee39de78f26339546cc3305d0d97
song <songsm5504@gmail.com>
2020-09-04 23:27:34 +0900

    17.1 컴포넌트 이벤트 만들기

--- src/App.js
+++ src/App.js
@@ -40,31 +40,15 @@ class App extends Component {
 
     return (
       <div>
-        {/* <Subject
+        <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-        /> */}
-
-        <header>
-          <h1>
-            <a
-              href="/"
-              onClick={function (e) {
-                console.log(e);
-                e.preventDefault();
-
-                // this.state.mode = "welcome";
-                this.setState({
-                  mode: "welcome",
-                });
-              }.bind(this)}
-            >
-              {this.state.subject.title}
-            </a>
-          </h1>
-          {this.state.subject.sub}
-        </header>
-
+          onChangePage={function () {
+            this.setState({
+              mode: "welcome",
+            });
+          }.bind(this)}
+        />
         <TOC data={this.state.contents} />
         <Content title={_title} desc={_desc} />
       </div>
