b87eee0c3e1498c606c8a697d9cbbfd7ffbce0c0
song <songsm5504@gmail.com>
2020-09-06 01:09:25 +0900

    19.3 create 구현 : mode 전화 기능2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Subject from "./components/Subject";
 import TOC from "./components/TOC";
 import Control from "./components/Control";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import "./App.css";
 
 class App extends Component {
@@ -29,10 +30,12 @@ class App extends Component {
     console.log("APP render");
 
     var _title,
-      _desc = null;
+      _desc,
+      _article = null;
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc} />;
     } else if (this.state.mode === "read") {
       var i = 0;
 
@@ -46,6 +49,9 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc} />;
+    } else if (this.state.mode === "create") {
+      _article = <CreateContent title={_title} desc={_desc} />;
     }
 
     return (
@@ -72,7 +78,7 @@ class App extends Component {
             });
           }.bind(this)}
         />
-        <Content title={_title} desc={_desc} />
+        {_article}
       </div>
     );
   }
